5c4e425e2d09f60292c94e159781fa3f5809e2f2
Harshita <hgupta5_be18@thapar.edu>
2024-01-01 22:08:25 +0530

    page routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import VideoDetails from "./components/VideoDetails";
+import ChannelDetails from "./components/ChannelDetails";
+
 function App() {
   return (
-    <div>
-      App - starting the new application -- welcome 
-    </div>
+    <BrowserRouter>
+      <Box sx={{ border: "4px solid red" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Homepage />} />
+          <Route path="/videoDetails/:id" exact element={<VideoDetails />} />
+          <Route path="/channelDetails/:id" exact element={<ChannelDetails />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
